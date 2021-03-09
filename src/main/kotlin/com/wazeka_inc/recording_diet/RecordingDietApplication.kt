f9864fe6c0fe340da229bd334efaf8c9434680d7
KAMAGASAKO Masatoshi <kamagasako@gmail.com>
2021-03-09 23:40:16 +0900

    Initial commit

---
+++ src/main/kotlin/com/wazeka_inc/recording_diet/RecordingDietApplication.kt
@@ -0,0 +1,11 @@
+package com.wazeka_inc.recording_diet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecordingDietApplication
+
+fun main(args: Array<String>) {
+    runApplication<RecordingDietApplication>(*args)
+}
---
+++ src/test/kotlin/com/wazeka_inc/recording_diet/RecordingDietApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wazeka_inc.recording_diet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecordingDietApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
